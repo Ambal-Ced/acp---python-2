c83f763f3260274dd6d68a16d3ea58c4c64f5b38
Ambal-Ced <147784891+Ambal-Ced@users.noreply.github.com>
2023-10-13 10:49:04 +0800

    Add files via upload

---
+++ LabEx2.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+print("Modulus")
+mod = num1%num2
+print(num1,"%",num2,"=",mod)
+print("Floor Division")
+fd = num1//num2
+print(num1,"//",num2,"=",fd)
